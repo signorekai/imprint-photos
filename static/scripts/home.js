3de1047c9265861587b7206f5fc8e312110c9fee
Alfred Lau <alfred@sulphur.com.sg>
2019-05-16 02:20:11 +0800

    use vanilla js

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,16 +1,35 @@
-jQuery( document ).ready( function( $ ) {
-  // Your JavaScript goes here
-  $nav = $('a.works__link');
+// jQuery( document ).ready( function( $ ) {
+//   // Your JavaScript goes here
+//   $nav = $('a.works__link');
+
+//   if ($nav) {
+//     $nav.hover( function() {
+//       $($(this).data('target')).addClass('overlay--show');
+//       $('.header__logo svg').addClass('--show');
+//       $('main article h1').addClass('h1--hide');
+//     }, function() {
+//       $($(this).data('target')).removeClass('overlay--show');
+//       $('.header__logo svg').removeClass('--show');
+//       $('main article h1').removeClass('h1--hide');
+//     });
+//   }
+
+// });
+
+ready(function() {
+  var $nav = f('a.works__link');
 
   if ($nav) {
-    $nav.hover( function() {
-      $($(this).data('target')).addClass('overlay--show');
-      $('.header__logo svg').addClass('--show');
-      $('main article h1').addClass('h1--hide');
-    }, function() {
-      $($(this).data('target')).removeClass('overlay--show');
-      $('.header__logo svg').removeClass('--show');
-      $('main article h1').removeClass('h1--hide');
+    $nav.on('mouseenter', function() {
+      f(this.data('target')).addClass('overlay--show');
+      f('.header__logo svg').addClass('--show');
+      f('main article h1').addClass('h1--hide');
+    });
+
+    $nav.on('mouseleave', function() {
+      f(this.data('target')).removeClass('overlay--show');
+      f('.header__logo svg').removeClass('--show');
+      f('main article h1').removeClass('h1--hide');
     });
   }
 
