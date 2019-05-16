49b83382a0e9fd883e58da4699eaa481b70a10d6
Alfred Lau <alfred@sulphur.com.sg>
2019-05-16 17:47:40 +0800

    BEM!

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -22,13 +22,13 @@ ready(function() {
   if ($nav) {
     $nav.on('mouseenter', function() {
       f(this.data('target')).addClass('overlay--show');
-      f('.header__logo svg').addClass('--show');
+      f('.header__svg').addClass('header__svg--show');
       f('main article h1').addClass('h1--hide');
     });
 
     $nav.on('mouseleave', function() {
       f(this.data('target')).removeClass('overlay--show');
-      f('.header__logo svg').removeClass('--show');
+      f('.header__svg').removeClass('header__svg--show');
       f('main article h1').removeClass('h1--hide');
     });
   }
