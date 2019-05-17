395927cca7f9ea3789545e02b72b9362a11dd9fc
Alfred Lau <alfred@sulphur.com.sg>
2019-05-17 13:15:38 +0800

    remove jQuery code

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,21 +1,3 @@
-// jQuery( document ).ready( function( $ ) {
-//   // Your JavaScript goes here
-//   $nav = $('a.works__link');
-
-//   if ($nav) {
-//     $nav.hover( function() {
-//       $($(this).data('target')).addClass('overlay--show');
-//       $('.header__logo svg').addClass('--show');
-//       $('main article h1').addClass('h1--hide');
-//     }, function() {
-//       $($(this).data('target')).removeClass('overlay--show');
-//       $('.header__logo svg').removeClass('--show');
-//       $('main article h1').removeClass('h1--hide');
-//     });
-//   }
-
-// });
-
 ready(function() {
   var $nav = f('a.works-nav__link');
 
