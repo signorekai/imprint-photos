14adc3767b3f84fc180a665b091d4ff0a729cb0a
Alfred Lau <alfred@sulphur.com.sg>
2019-06-21 17:15:33 +0800

    added SEO description to settings

--- inc/customizer.php
+++ inc/customizer.php
@@ -2,14 +2,22 @@
 /**
 * Create Logo Setting and Upload Control
 */
-function add_customizer_settings($wp_customize) {
-  $wp_customize->add_section( 'page_options', array(
+function add_customizer_settings($wpc) {
+  $wpc->add_section( 'page_options', array(
     'title' => 'Page Options',
     'priority' => 30,
   ));
 
-  $wp_customize->add_setting('footer_text');
-  $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'footer_text',
+  $wpc->add_setting('seo_description');
+  $wpc->add_control( new WP_Customize_Control($wpc, 'seo_description', array(
+    'label' => 'Description',
+    'section' => 'title_tagline',
+    'settings' => 'seo_description',
+    'type' => 'textarea',
+  )));
+
+  $wpc->add_setting('footer_text');
+  $wpc->add_control( new WP_Customize_Control( $wpc, 'footer_text',
     array(
     'label' => 'Footer Text',
     'section' => 'page_options',
@@ -18,8 +26,8 @@ function add_customizer_settings($wp_customize) {
     ) ) 
   );
 
-  $wp_customize->add_setting('footer_bg');
-  $wp_customize->add_control( new WP_Customize_Upload_Control( $wp_customize, 'footer_bg',
+  $wpc->add_setting('footer_bg');
+  $wpc->add_control( new WP_Customize_Upload_Control( $wpc, 'footer_bg',
     array(
     'label' => 'Footer Background Image',
     'section' => 'page_options',
