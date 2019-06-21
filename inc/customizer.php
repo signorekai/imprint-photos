a3ea7fc599e3ed8c127e4b964df474dafb0d8fb8
Alfred Lau <alfred@sulphur.com.sg>
2019-06-21 17:23:22 +0800

    added Opengraph featured image

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,20 +1,41 @@
 <?php
-/**
-* Create Logo Setting and Upload Control
-*/
 function add_customizer_settings($wpc) {
-  $wpc->add_section( 'page_options', array(
-    'title' => 'Page Options',
-    'priority' => 30,
+
+  /**
+   * Add "SEO" section
+   */
+
+  $wpc->add_section( 'seo', array(
+    'title' => __('SEO Settings', 'winterfell'),
+    'priority' => 35,
   ));
 
   $wpc->add_setting('seo_description');
   $wpc->add_control( new WP_Customize_Control($wpc, 'seo_description', array(
-    'label' => 'Description',
-    'section' => 'title_tagline',
+    'label' => __('Description', 'winterfell'),
+    'section' => 'seo',
     'settings' => 'seo_description',
     'type' => 'textarea',
   )));
+
+  $wpc->add_setting('seo_photo');
+  $wpc->add_control( new WP_Customize_Upload_Control( $wpc, 'seo_photo',
+    array(
+    'label' => __('Cover Image', 'winterfell'),
+    'description' => __('Select the cover image that will be displayed when shared on social media', 'winterfell'),
+    'section' => 'seo',
+    'settings' => 'seo_photo',
+    ) ) 
+  );
+
+  /*
+   * Add "Page Options" section to control page design settings
+   */
+
+  $wpc->add_section( 'page_options', array(
+    'title' => __('Page Options', 'winterfell'),
+    'priority' => 30,
+  ));
 
   $wpc->add_setting('footer_text');
   $wpc->add_control( new WP_Customize_Control( $wpc, 'footer_text',
