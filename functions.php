735fae41f6b2469ec31198a6c4a23645615d9301
Alfred Lau <alfred@sulphur.com.sg>
2019-06-26 15:01:39 +0800

    added wordpress cleanup

--- functions.php
+++ functions.php
@@ -43,6 +43,7 @@ class StarterSite extends Timber\Site {
 	public function __construct() {
 		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
 		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
+		add_action( 'after_setup_theme', array( $this, 'wordpress_cleanup' ) );
 		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
 		add_action( 'init', array( $this, 'register_post_types' ) );
 		add_action( 'init', array( $this, 'register_taxonomies' ) );
@@ -64,7 +65,13 @@ class StarterSite extends Timber\Site {
 	}
 	/** This is where you can register custom taxonomies. */
 	public function register_taxonomies() {
+	}
 
+	public function wordpress_cleanup() {
+		remove_action('wp_head', 'wlwmanifest_link'); 							// remove support to Windows Live Writer
+		remove_action('wp_head', 'wp_generator'); 									// remove wordpress version in header
+		remove_action('wp_head', 'rsd_link'); 											// remove XML-RPC RSD link
+		remove_action('wp_head', 'rest_output_link_wp_head', 10);		// disable WP REST API
 	}
 
 	/**
@@ -118,10 +125,11 @@ class StarterSite extends Timber\Site {
 
 		$theme = new Timber\Theme();
 		$context['footer_text'] = $theme->theme_mod('footer_text');
-		$context['footer_bg'] = $theme->theme_mod('footer_bg');
+		$context['footer_bg'] = new Timber\Image($theme->theme_mod('footer_bg'));
+		$context['test'] = "test";
 		$context['footer'] = array(
 			'text' => $theme->theme_mod('footer_text'),
-			'bg' => $theme->theme_mod('footer_bg'),
+			'bg' => new Timber\Image($theme->theme_mod('footer_bg')),
 		);
 		$context['seo'] = array(
 			'description' => $theme->theme_mod('seo_description'),
@@ -196,7 +204,7 @@ class StarterSite extends Timber\Site {
 			)
 		);
 
-		add_image_size('full', 1920, 1080);
+		add_image_size('hd', 1920, 1080);
 	}
 
 	/** This Would return 'foo bar!'.
